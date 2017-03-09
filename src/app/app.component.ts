56f60976741b0564b122d09f6adf0a82039d28e6
Lawrence Martin <lawrence.martin@cantire.com>
2017-03-09 10:20:05 -0500

    white screen after spalsh bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,13 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      Splashscreen.hide();
+      this.hideSplashScreen();
     });
   }
+  hideSplashScreen() {
+if (Splashscreen) {
+setTimeout(() => {
+Splashscreen.hide();
+}, 100);
+}}
 }
